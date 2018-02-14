5d0b63cdb21355a4a48fd106e84165d3bd40b6fd
Charley Oliveira <charleycesar@gmail.com>
2018-02-14 15:29:50 -0200

    setup redux in application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
@@ -19,8 +20,9 @@ class App extends Component {
           firebase.initializeApp(config);
     }
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
-            <Provider store={createStore(reducers)}>
+            <Provider store={store}>
                 <LoginForm />
             </Provider>
         );
